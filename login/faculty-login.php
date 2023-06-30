66b62309fa1a487b058c53c65b0559c58bd7e55d
jnlglr <janel.aguilar.ja@gmail.com>
2023-07-01 00:27:36 +0800

    Create faculty-login.php

---
+++ login/faculty-login.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Login</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Antique&display=swap" rel="stylesheet"> 
+        <div class="loginbox">
+            <h1>Professor Log-In</h1>
+            <form>
+                <p>Username</p>
+                <input type="text" name="username" placeholder="Enter Username">
+                <p>Password</p>
+                <input type="password" name="password" placeholder="Enter Password">
+                <input type="submit" name="" value="Login"><br>
+                <a href="#">Forget password?</a><br>
+                <a href="../registration/prof-registration.php">Create an account</a>
+                <br>
+                <a href="../index.php">Home</a>
+            </form>
+        </div>
+        </div>
+    <body>
+    </head>
+
+    </body>
+</html>
